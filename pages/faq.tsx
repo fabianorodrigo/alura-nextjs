9cf86137c5de5e9a6386791a0ad07a6f75758467
Fabiano Nascimento <fabianorodrigo@gmail.com>
2022-03-16 08:43:44 -0300

    Atualização estilo página FAQ

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,55 +1,28 @@
-import type { NextPage } from 'next'
-import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import Link from '../src/components/Link';
+import FAQScreen from '../src/screens/FAQScreen';
 
 // SSG - Static Site Generation
 // SSR - Server Side Rendering
 // ISG - Incremental Static Generation
 
-export async function getStaticProps(){
-  console.log('Em modo DEV, sempre roda! A cada acesso')
-  console.log('Roda SOMENTE em build time')
-  const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+export async function getStaticProps() {
+  console.log("Em modo DEV, sempre roda! A cada acesso");
+  console.log("Roda SOMENTE em build time");
+  const FAQ_API_URL =
+    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
   const faq = await fetch(FAQ_API_URL)
-      .then((respostaDoServidor) => {
-          return respostaDoServidor.json();
-      })
-      .then((resposta) => {
-          return resposta;
-      });
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json();
+    })
+    .then((resposta) => {
+      return resposta;
+    });
 
   return {
     props: {
-        qualquercoisa: 'que eu passar aqui',
-        faq,
+      qualquercoisa: "que eu passar aqui",
+      faq,
     },
   };
 }
 
-const FAQ: NextPage = (props: any) => {
-  console.log('Isso roda no servidor???')
-    return (
-        <div>
-            <Head>
-                <title>FAQ - Alura Cases Campanha</title>
-            </Head>
-            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
-            <Link href="/">
-                Ir para a home
-            </Link>
-            <ul>
-                {props.faq.map((item: {question: any, answer: any}) => (
-                    <li key={item.question}>
-                        <article>
-                            <h2>{item.question}</h2>
-                            <p>{item.answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
-
-export default FAQ;
+export default FAQScreen;
